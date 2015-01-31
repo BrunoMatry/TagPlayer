41ef093e150c1ea47a93a34715a26a62f87ebf6c
Bruno Matry <brunomatry@gmail.com>
2015-01-31 20:49:13 +0100

    Log improvement

--- src/model/Log.java
+++ src/model/Log.java
@@ -1,11 +1,25 @@
 package model;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Log {
+    private static DateFormat dateFormat = new SimpleDateFormat("[yyyy/MM/dd HH:mm:ss]");
+    
     public static void log(String s) {
         System.out.println(s);
     }
     
+    private static String getTime() {
+        return dateFormat.format(new Date());
+    }
+    
     public static void addTagLog(String musicFileName, String tagName) {
-        Log.log("[Tag] \"" + tagName + "\" added on file \"" + musicFileName + "\"");
+        Log.log("[Tag]" + getTime() + " \"" + tagName + "\" added on file \"" + musicFileName + "\"");
+    }
+
+    public static void onCloseLog() {
+        Log.log("[Closed]" + getTime() + " Successfully saved database");
     }
 }
--- src/view/OnCloseHandler.java
+++ src/view/OnCloseHandler.java
@@ -3,6 +3,7 @@ package view;
 import javafx.application.Platform;
 import javafx.event.EventHandler;
 import javafx.stage.WindowEvent;
+import model.Log;
 
 
 public class OnCloseHandler implements EventHandler<WindowEvent> {
@@ -13,6 +14,7 @@ public class OnCloseHandler implements EventHandler<WindowEvent> {
     @Override
     public void handle(WindowEvent t) {
         // TODO Sauvegarde de la DBA
+        Log.onCloseLog();
         Platform.exit();
         System.exit(0);
     }
