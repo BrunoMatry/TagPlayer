d703f5eccca74fd53ad7f3de473f5496f8ab1071
Bruno Matry <brunomatry@gmail.com>
2015-01-31 20:18:53 +0100

    All Items Button (optimized)

--- src/view/window/MainWindow.java
+++ src/view/window/MainWindow.java
@@ -10,11 +10,13 @@ import view.MyListView;
 import view.MyToolBar;
 
 public class MainWindow {
+
     private Stage primaryStage;
     private final BorderPane root;
     private final StackPane musicFileStackPane;
     private final Scene scene;
     private static final MainWindow INSTANCE = new MainWindow();
+    private MyListView originalListView;
     private MusicFileList originalMusicFileList;
     private MusicFileList currentMusicFileList;
     
@@ -36,7 +38,8 @@ public class MainWindow {
         INSTANCE.currentMusicFileList = list;
         INSTANCE.primaryStage.setTitle(ApplicationParameters.APPLICATION_NAME);
         INSTANCE.primaryStage.setScene(INSTANCE.scene);
-        setMusicFileList(INSTANCE.originalMusicFileList);
+        INSTANCE.originalListView = new MyListView(INSTANCE.originalMusicFileList.getMusicFiles());
+        INSTANCE.musicFileStackPane.getChildren().add(INSTANCE.originalListView);
         INSTANCE.primaryStage.show();
     }
 
@@ -46,6 +49,11 @@ public class MainWindow {
         INSTANCE.currentMusicFileList = filteredList;
     }
     
+    public static void setOriginalMusicFileList() {
+        INSTANCE.musicFileStackPane.getChildren().clear();
+        INSTANCE.musicFileStackPane.getChildren().add(INSTANCE.originalListView);
+    }
+
     public static MusicFileList getOriginalMusicFileList() {
         return INSTANCE.originalMusicFileList;
     }
